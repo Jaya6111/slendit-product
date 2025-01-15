0cfb7da9f88e0f7da2131bef9887f5be287848a8
Jaya6111 <136075867+Jaya6111@users.noreply.github.com>
2025-01-15 10:05:38 +0530

    Created product service
    Uploading the product service

---
+++ slendit-product/src/main/java/com/fq/slendit/product/SlenditProductApplication.java
@@ -0,0 +1,13 @@
+package com.fq.slendit.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlenditProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlenditProductApplication.class, args);
+	}
+
+}
---
+++ slendit-product/src/test/java/com/fq/slendit/product/SlenditProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fq.slendit.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlenditProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
